065cd3c77eee2a544e5e87d8224ff5af4cfb93cd
Vera <veronika.kozel.97@mail.ru>
2023-07-01 15:41:59 +0200

    Добавили файл

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает (N), а на выходе показывает все четные числа от 1 до N
+// 5 -> 2,4
+// 8 -> 2,4,6,8
+
+Console.WriteLine("Введите число => ");         // Вывести приглашение на консоль
+string inputedString = Console.ReadLine();     // Ввод пользователя в строку
+int number2 = Convert.ToInt32(inputedString);   // Приеобразование введенной строки в число
+int cnt = 1;
+while(cnt <= number2) 
+{
+    if (cnt % 2 == 0)
+    {
+        Console.WriteLine(cnt + " ");
+    }
+    cnt = cnt+1 ;
+}
+
+
+
+
